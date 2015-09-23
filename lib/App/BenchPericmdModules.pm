0ff1e9e608def0d401523cb8ce7dc6892376d9a0
perlancar (@pc-office) <perlancar@gmail.com>
2015-09-23 10:32:00 +0700

    Initial commit

---
+++ lib/App/BenchPericmdModules.pm
@@ -0,0 +1,13 @@
+package App::BenchPericmdModules;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Benchmark various Perinci::CmdLine::* frameworks
+
+=head1 SYNOPSIS
+
+See the included scripts:
+
+#INSERT_EXECS_LIST
